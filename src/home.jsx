d16453058642b3de8301d935ca99f00a31724663
Sonia <sonia@192-168-1-109.tpgi.com.au>
2023-03-13 01:33:13 +1100

    more progress on About and Contact Page

--- src/home.jsx
+++ src/home.jsx
@@ -2,17 +2,13 @@ const home = document.getElementById("home-button");
 const about = document.getElementById("about-button");
 const projects = document.getElementById("projects-button");
 const contact = document.getElementById("contact-button");
-// let curr = home;
+const send = document.getElementById("send-button");
 
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0)
+}
 home.addEventListener('click', ()=> {
-    // document.getElementById('home-page').style.display = 'block';
     document.getElementById('home-line').style.display = 'block';
-    // home.style.color = '#1a40eb';
-    // curr.style.color = '#7a7a7b';
-    // curr = home;
-    // document.getElementById('about-page').style.display = 'none';
-    // document.getElementById('projects-page').style.display = 'none';
-    // document.getElementById('contact-page').style.display = 'none';
 
     document.getElementById('about-line').style.display = 'none';
     document.getElementById('projects-line').style.display = 'none';
@@ -20,24 +16,7 @@ home.addEventListener('click', ()=> {
     document.getElementById('home-content').scrollIntoView({behavior: 'smooth', block: 'center'});
 });
 
-// about.addEventListener('click', ()=> {
-//     document.getElementById('about-page').style.display = 'block';
-//     document.getElementById('about-line').style.display = 'block';
-
-//     document.getElementById('home-page').style.display = 'none';
-//     document.getElementById('projects-page').style.display = 'none';
-//     document.getElementById('contact-page').style.display = 'none';
-
-//     document.getElementById('home-line').style.display = 'none';
-//     document.getElementById('projects-line').style.display = 'none';
-//     document.getElementById('contact-line').style.display = 'none';
-// });
-
 about.addEventListener('click', ()=> {
-    // var element = document.getElementById("div");
-    // about.style.color = '#1a40eb';
-    // curr.style.color = '#7a7a7b';
-    // curr = about;
 
     document.getElementById('about-line').style.display = 'block';
     document.getElementById('about-content').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'center'});
@@ -47,23 +26,9 @@ about.addEventListener('click', ()=> {
     document.getElementById('contact-line').style.display = 'none';
 });
 
-// $("button").click(function() {
-//     $('html,body').animate({
-//         scrollTop: $(".second").offset().top},
-//         'slow');
-// });
-
 projects.addEventListener('click', ()=> {
-    // projects.style.color = '#1a40eb';
-    // curr.style.color = '#7a7a7b';
-    // curr = projects;
-    // document.getElementById('projects-page').style.display = 'block';
     document.getElementById('projects-line').style.display = 'block';
     document.getElementById('projects-content').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'center'});
-
-    // document.getElementById('home-page').style.display = 'none';
-    // document.getElementById('about-page').style.display = 'none';
-    // document.getElementById('contact-page').style.display = 'none';
 
     document.getElementById('home-line').style.display = 'none';
     document.getElementById('about-line').style.display = 'none';
@@ -71,18 +36,31 @@ projects.addEventListener('click', ()=> {
 });
 
 contact.addEventListener('click', ()=> {
-    // contact.style.color = '#1a40eb';
-    // curr.style.color = '#7a7a7b';
-    // curr = contact;
-    // document.getElementById('contact-page').style.display = 'block';
     document.getElementById('contact-line').style.display = 'block';
     document.getElementById('contact-content').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'center'});
-    
-    // document.getElementById('home-page').style.display = 'none';
-    // document.getElementById('about-page').style.display = 'none';
-    // document.getElementById('projects-page').style.display = 'none';
 
     document.getElementById('home-line').style.display = 'none';
     document.getElementById('about-line').style.display = 'none';
     document.getElementById('projects-line').style.display = 'none';
 });
+
+send.addEventListener('click', () => {
+    let sendTo = "sonia899998@gmail.com";
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let sub = document.getElementById('subject').value;
+
+    let text = document.getElementById('message');
+    let body = ""+text.value+'%0A%0A%0A';
+        body += "From："+name+'%0A';
+        body += "Email："+email+'%0A';
+
+    document.getElementById('mailto').href = "mailto:" + sendTo + "?subject=" + sub + "&body=" + body;
+    document.getElementById('mailto').click();
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('message').value = '';
+})
